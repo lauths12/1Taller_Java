1c5b8084665cb8ab049af7f9ab6449097188ff54
lauths12 <l.carretero@uniandes.edu.co>
2023-01-31 20:51:29 -0500

    Entrega_Final

--- Taller0_Olimpicos/src/uniandes/dpoo/taller0/modificacion/Modificacion.java
+++ Taller0_Olimpicos/src/uniandes/dpoo/taller0/modificacion/Modificacion.java
@@ -7,6 +7,7 @@ import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
 
 public class Modificacion {
 	public static void main (String[] args) throws FileNotFoundException, IOException {
+		System.out.println("Hola, Mundo!");
 		CalculadoraEstadisticas calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
 		System.out.println(calc.paisConMasMedallistas());
 	}
